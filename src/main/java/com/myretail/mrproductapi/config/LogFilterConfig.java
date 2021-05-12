41e76a68c1c6900ac1cb59350f1d63d8927b160a
Narayanan Lakshmi <narayanan.lakshmi@tr.com>
2021-05-12 10:37:12 -0700

    docker file and config file rename

--- src/main/java/com/myretail/mrproductapi/config/LogFilterConfig.java
+++ src/main/java/com/myretail/mrproductapi/config/LogFilterConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
 @Configuration
-public class RequestLogFilterConfig {
+public class LogFilterConfig {
     @Bean
     CommonsRequestLoggingFilter loggingFilter() {
         CommonsRequestLoggingFilter filter = new CommonsRequestLoggingFilter();
