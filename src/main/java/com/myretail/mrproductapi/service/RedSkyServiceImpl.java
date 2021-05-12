a5806de6d3d58fd19c44343f33d63a11e150dd2c
Narayanan Lakshmi <narayanan.lakshmi@tr.com>
2021-05-12 07:21:12 -0700

    handle recovery of red sky call

--- src/main/java/com/myretail/mrproductapi/service/RedSkyServiceImpl.java
+++ src/main/java/com/myretail/mrproductapi/service/RedSkyServiceImpl.java
@@ -2,6 +2,8 @@ package com.myretail.mrproductapi.service;
 
 import com.myretail.mrproductapi.domain.redsky.RedSkyResponse;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.remoting.RemoteAccessException;
 import org.springframework.retry.annotation.Backoff;
 import org.springframework.retry.annotation.Recover;
@@ -16,6 +18,7 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class RedSkyServiceImpl implements RedSkyService {
     private final RestTemplate restTemplate;
+    private final Logger logger = LoggerFactory.getLogger(RedSkyServiceImpl.class);
 
     @Override
     @Retryable(include = {HttpClientErrorException.class, RemoteAccessException.class}, backoff = @Backoff(delay = 300, maxDelay = 500))
@@ -24,7 +27,14 @@ public class RedSkyServiceImpl implements RedSkyService {
     }
 
     @Recover
-    public Optional<?> recover() {
+    public Optional<?> recover(HttpClientErrorException ex, Integer id) {
+        logger.error("Product title not found for product with id: " + id);
+        return Optional.empty();
+    }
+
+    @Recover
+    public Optional<?> recover(RemoteAccessException ex, Integer id) {
+        logger.error("Could not reach red sky service to retrieve title for product with id: " + id);
         return Optional.empty();
     }
 
